492d49bc24931b41bbf86eaab836b6714c3a34fa
Iqbal Febrianwar <85827703+IqbalFebrianwar@users.noreply.github.com>
2023-12-07 19:10:23 +0700

    Delete src/app/(public)/auth directory

--- src/app/(public)/auth/forgot-password/page.jsx
+++
@@ -1,8 +0,0 @@
-const ForgotPasswordPage = () => {
-  return (
-    <div className="w-full py-28 grid h-screen bg-secondary px-10">
-      <h1>Forgot pass</h1>
-    </div>
-  );
-};
-export default ForgotPasswordPage;
